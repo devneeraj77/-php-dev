3ea0014a30d450c90e03f2db40ea1e49ebe672e9
devneeraj77 <dev.neerajrekwar@gmail.com>
2025-08-22 16:15:40 +0530

    Update carousel.php

--- carousel.php
+++ carousel.php
@@ -5,7 +5,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="/styles.css">
+    <link rel="stylesheet" href="/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.0/css/all.min.css" integrity="sha512-DxV+EoADOkOygM4IR9yXP8Sb2qwgidEmeqAEmDKIOfPRQZOWbXCzLC6vjbZyy0vPisbH2SyW27+ddLVCN+OMzQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet"
         href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css" />
     <link rel="stylesheet"
@@ -15,43 +16,235 @@
 </head>
 
 <body>
-    <div class="d-flex position-relative overflow-hidden justify-content-center align-items-center" style="min-height: 50vh;">
-        
-        <?php
-        $url = 'https://reqres.in/api/users?page=2';
-        $response = file_get_contents($url);
-        $data = json_decode($response, true);
-        $users = $data['data'];
-        ?> 
-          <?php foreach ($users as $user): ?>
-                <div class="bg-light rounded px-4 py-3 overflow-hidden text-center d-flex flex-column justify-content-center align-items-center">
-                    <div class="" style="">
-                        <div class="d-flex flex-column justify-content-start align-items-start">
-                            <i class="fa-solid fa-3x text-primary fa-quote-left"></i>
-                            <em><i>Efficient, thorough, and highly professional service. They eliminated the pests and left my home sparkling clean. Highly recommended!</i>
-                        </div></em>
-                        <div class="py-1 pt-3 px-2 gap-2  d-flex" aria-label="<?= htmlspecialchars($s['first_name']) ?>">
-                            <div class="rounded-circle overflow-hidden" style="height: 44px; width: 44px">
-                                <img src="<?= htmlspecialchars($user['avatar']) ?>" sizes="(max-width: 768px) 92vw, (max-width: 1200px) 80vw, 60vw"
-                                    width="50" height="50" alt="avatar" width="50" />
-                            </div>
-                            <div class="">
-                                <div class="d-flex text-primary">
-                                    <?= htmlspecialchars($user['first_name']) ?>
-                                    <?= htmlspecialchars($user['last_name']) ?>
-                                </div>
-                                <div>
-                                    <?= htmlspecialchars($user['email']) ?>
-                                </div>
-                            </div>
+    <!DOCTYPE html>
+    <html lang="en">
+
+    <head>
+        <meta charset="utf-8">
+        <title>PestKit - Pest Control Website Template</title>
+        <meta content="width=device-width, initial-scale=1.0" name="viewport">
+        <meta content="" name="keywords">
+        <meta content="" name="description">
+    </head>
+
+    <body>
+        <!-- Page Header Start -->
+        <section class="bg-light d-flex flex-colum justify-content-center align-items-center overflow-hidden">
+            <div class="container-lg mx-auto row g-4 mb-5 py-2">
+                <div class="py-4 col-md p-md-4">
+                    <h1 class="dispaly-4 text-muted pt-md-5 py-2">Unlock More: Our Multi-Service Plumbing Deals</h1>
+                    <!--<h1 class="dispaly-4 text-muted pt-md-5 py-2">Unlock More: Our Multi-Service Plumbing Deals</h1>-->
+                    <p class="fs-5 fs-md-4  text-muted ">We're committed to delivering high-quality service with a personal touch, ensuring your home's plumbing is always running smoothly.</p>
+                    <ol class="list-unstyled px-3 py-3">
+                        <li><i class="fas fa-check text-success"></i> Fast, Reliable, and Affordable Plumbing Solutions.</li>
+                        <li><i class="fas fa-check text-success"></i> Local plumbing specialists</li>
+                        <li><i class="fas fa-check text-success"></i> 24/7 Emergency Service</li>
+                    </ol>
+                    <div class="d-inline gap-2">
+                        <a href="tel:+18885971392" class="btn btn-primary px-3 py-2 border-0 text-dark text-decoration-none text-capitalize">Call Now</a>
+                        <!--<a href="/" class="anchor text-muted"></a>-->
+                    </div>
+                </div>
+                <div class=" col-md p-md-4">
+                    <div class="d-flex h-100 align-items-center justify-content-center">
+                        <div class="pt-md-5 py-2">
+                            <img src="https://fixitfusion.com/img/plumber-img-1.jpg" class="img-fluid shadow" alt="Plumbing Services">
+
                         </div>
                     </div>
                 </div>
-            <?php endforeach; ?>
+            </div>
+        </section>
+        <section class="py-5 d-flex flex-colum justify-content-center align-items-center overflow-hidden">
+            <div class="container-lg py-5">
+                <h3 class="display-6 text-muted text-end pt-md-5 p-3 py-2">
+                    Our Plumbing Services
+                </h3>
+                <div class="d-lg-flex gap-2 my-5 text-center justify-content-center align-items-center">
+                    <div class="p-3">
+                        <i class="fa-solid fa-3x fa-rotate-left text-secondary py-3"></i>
+                        <h5 class="fs-3 text-muted">8+ Integration</h5>
+                        <p class="fs-5 px-4 px-md-1 text-emphasis">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Porro, labore?</p>
+                    </div>
+                    <div class="p-3">
+                        <i class="fa-solid fa-medal fa-solid fa-3x text-secondary py-3"></i>
+                        <h5 class="fs-3 text-muted">High Quality</h5>
+                        <p class="fs-5 px-4 px-md-1 text-emphasis">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolores, deleniti.</p>
+                    </div>
+                    <div class="p-3">
+                        <i class="fa-solid fa-certificate fa-3x text-secondary py-3"></i>
+                        <h5 class="fs-3 text-muted">Certified Professionals</h5>
+                        <p class="fs-5 px-4 px-md-1 text-emphasis">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet, quibusdam!</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="bg-light py-5 d-flex flex-colum justify-content-center align-items-center overflow-hidden">
+            <div class="container-lg py-5 py">
+                <h3 class="display-6 text-muted text-start pt-md-5 p-3 py-2 ">
+                    What Our Customers Say
+                </h3>
+                <div class=" py-4 text-muted px-md-5 mx-md-5 ">
+                    <div class="container-lg bg-muted my-4 d-sm-flex px-md-5 mx-5 justify-content-center mx-auto gap-3 align-items-center">
+                        <div class="rounded-pill  py-3">
+                            <!--<img src="https://i.pravatar.cc/150?u" class="rounded-pill" width="32" height="32" alt="Hero Image 1" >-->
+                            <div class="rounded-circle mx-auto overflow-hidden" style="height: 140px; width: 140px">
+                                <img src="https://fixitfusion.com/img/testimonial-1.jpg" sizes="(max-width: 768px) 22vw, (max-width: 1200px) 80vw, 60vw" width="140" height="140" alt="avatar">
+                            </div>
+                        </div>
+                        <figure class=" text-end px-2 pt-3 ">
+                            <blockquote class="blockquote ">
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, nobis velit. Ratione nihil eligendi nulla nostrum, maiores quia velit aut!</p>
+                            </blockquote>
+                            <figcaption class="blockquote-footer text-secondary">
+                                New York in <cite title="Source Title ">James St.</cite>
+                            </figcaption>
+                        </figure>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="py-5 d-flex flex-colum justify-content-center align-items-center overflow-hidden">
+            <div class="container-lg py-5">
+                <h3 class="display-6  text-center pt-md-5 p-3 py-2">
+                    Your Local Plumbing Experts
+                </h3>
+                <div class="row g-5 mx-auto py-5 text-center">
+                    <div class="p-3 col-lg-4 col-md-6 col-sm-12">
+                        <div class="ratio ratio-4x3">
+                            <div class="mx-auto overflow-hidden ratio ratio-4x3">
+                                <img class="ratio ratio-4x3 " src="https://fixitfusion.com/img/plumber-img-1.jpg"
+                                    sizes="(max-width: 768px) 22vw, (max-width: 1200px) 80vw, 60vw"
+                                    width="300"
+                                    alt="avatar">
+                            </div>
 
-        </div>
-    </div>
-    </div>
+                        </div>
+                        <h5 class="pt-3 fw-bold">Amet consectetur</h5>
+                        <p class=" mx-auto fs-6 text-muted ratio ratio-3x4">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, nobis velit. Ratione nihil eligendi nulla nostrum, maiores quia velit aut!
+                        </p>
+                    </div>
+
+
+                    <div class="p-3 col-lg-4 col-md-6 col-sm-12">
+                        <div class="ratio ratio-4x3">
+                            <div class="mx-auto overflow-hidden ratio ratio-4x3 ">
+                                <img class="ratio ratio-4x3" src="https://fixitfusion.com/img/plumber-img-2.jpg"
+                                    sizes="(max-width: 768px) 22vw, (max-width: 1200px) 80vw, 60vw"
+                                    width="300"
+                                    alt="avatar">
+                            </div>
+
+                        </div>
+                        <h5 class="pt-3 fw-bold">Amet consectetur</h5>
+                        <p class=" mx-auto fs-6 text-muted ">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, nobis velit. Ratione nihil eligendi nulla nostrum, maiores quia velit aut!
+                        </p>
+                    </div>
+                    <div class="p-3 col-lg-4 col-md-6 col-sm-12">
+                        <div class="ratio ratio-4x3">
+                            <div class="mx-auto overflow-hidden ">
+                                <img class="ratio ratio-4x3" src="https://fixitfusion.com/img/plumber-img-3.jpg"
+                                    sizes="(max-width: 768px) 22vw, (max-width: 1200px) 80vw, 60vw"
+                                    width="300"
+                                    alt="avatar">
+                            </div>
+
+                        </div>
+                        <h5 class="pt-3 fw-bold">Amet consectetur</h5>
+
+                        <p class=" mx-auto fs-6 text-muted ratio ratio-3x4">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, nobis velit. Ratione nihil eligendi nulla nostrum, maiores quia velit aut!
+                        </p>
+                    </div>
+
+                </div>
+
+
+            </div>
+        </section>
+        <section class="py-5 d-flex flex-colum justify-content-center align-items-center overflow-hidden">
+            <div class="container-lg py-5">
+                <h3 class="display-6  text-start pt-md-5 p-3 py-2">
+                    Get In Touch
+                </h3>
+                <div class="row g-5 gap-5 mx-auto py-5">
+                    <form class="col-md border-2 py-3 my-5 rounded-2 bg-light  border-primary">
+                        <h6></h6>
+                        <div class="row">
+                            <div class="col-sm-6">
+                                <label for="inputEmail4" class="form-label">Email</label>
+                                <input type="email" class="form-control" id="inputEmail4">
+                            </div>
+                            <div class="col-sm-6">
+                                <label for="inputPhone" class="form-label">Phone</label>
+                                <input type="text" class="form-control" id="inputPhone">
+                            </div>
+                        </div>
+                        <div class="col-12">
+                            <label for="inputAddress" class="form-label">Address</label>
+                            <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St">
+                        </div>
+                        <!-- <div class="col-12">
+                            <label for="inputAddress2" class="form-label">Address 2</label>
+                            <input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor">
+                        </div> -->
+                        <div class="row">
+                            <div class="col-sm-6">
+                                <label for="inputCity" class="form-label">City</label>
+                                <input type="text" class="form-control" id="inputCity">
+                            </div>
+                            <div class="col-sm-6">
+                                <label for="inputState" class="form-label">State</label>
+                                <select id="inputState" class="form-select">
+                                    <option selected>Choose...</option>
+                                    <option>New York</option>
+                                    <option>California</option>
+                                </select>
+                            </div>
+                        </div>
+                        <!-- <div class="col-md-2">
+                            <label for="inputZip" class="form-label">Zip</label>
+                            <input type="text" class="form-control" id="inputZip">
+                        </div> -->
+                        <div class="col-12">
+                            <div class="form-check">
+                                <input class="form-check-input" type="checkbox" id="gridCheck">
+                                <label class="form-check-label" for="gridCheck">
+                                    Check me out
+                                </label>
+                            </div>
+                        </div>
+                        <div class="col-12">
+                            <button type="submit" class="btn btn-primary">Submit</button>
+                        </div>
+                    </form>
+                    <div class="col-md hello my-5  " style="min-height: 34vh">
+                        <h6 class="h4">Transform Your Home. Renew Your Life.</h6>
+                        <p class="">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ratione mollitia autem ex tempora, maiores non harum iste quae ea eveniet doloremque velit beatae doloribus. Atque.</p>
+                        <p class="">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ratione mollitia autem ex tempora, maiores non harum iste quae ea eveniet doloremque velit beatae doloribus. Atque.</p>
+                    </div>
+                </div>
+        </section>
+
+        <!-- Footer End -->
+
+        <!-- Back to Top -->
+
+        <!-- JavaScript Libraries -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
+        <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
+        <script src="lib/wow/wow.min.js"></script>
+        <script src="lib/easing/easing.min.js"></script>
+        <script src="lib/waypoints/waypoints.min.js"></script>
+        <script src="lib/owlcarousel/owl.carousel.min.js"></script>
+        <!-- Template Javascript -->
+        <script src="js/main.js"></script>
+    </body>
+
+    </html>
     <!-- jQuery (required) and OwlCarousel2 JS (CDN) -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
